2825554199186f84058dab83bd08a5164c3c1806
booths12556 <github_sso@cvrt.eu.org>
2025-04-04 12:45:52 +0000

    更新：index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const { PostalMime } = require('postal-mime');
+
+
+async function streamToArrayBuffer(stream, streamSize) {
+	let result = new Uint8Array(streamSize);
+	let bytesRead = 0;
+	const reader = stream.getReader();
+	while (true) {
+		const { done, value } = await reader.read();
+		if (done) {
+			break;
+		}
+		result.set(value, bytesRead);
+		bytesRead += value.length;
+	}
+	return result;
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+	async email(message, env, ctx) {
+		let from = message.headers.get("from");
+		let to = message.headers.get("to");
+		let subject = message.headers.get("subject");
+		let data = "";
+		if (message.raw != null) {
+			const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
+			const parser = new PostalMime.default();
+			const parsedEmail = await parser.parse(rawEmail);
+			data = parsedEmail.html;
+		}
+		let response = await fetch("https://example.com/email", {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json; chatset=UTF-8"
+			},
+			body: JSON.stringify({
+				"from": from,
+				"to": to,
+				"subject": subject,
+				"data": data
+			})
+		});
+		console.log(response.status, response.statusText, await response.text());
+	}
+}
